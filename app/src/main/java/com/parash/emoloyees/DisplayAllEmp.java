a8283f72c4fe008bf3d65e2be62ee1b9b0febfe8
parashghale <parash.ghale20@gmail.com>
2019-12-22 21:48:43 +0545

    Initial commit

---
+++ app/src/main/java/com/parash/emoloyees/DisplayAllEmp.java
@@ -0,0 +1,14 @@
+package com.parash.emoloyees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayAllEmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_all_emp);
+    }
+}
---
+++ app/src/main/java/com/parash/emoloyees/UpdateDeleteEmpActivity.java
@@ -0,0 +1,14 @@
+package com.parash.emoloyees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateDeleteEmpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delete_emp);
+    }
+}
